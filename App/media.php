2a589d051172fbe914c10153e2848f20833fe14b
Pedro Matheus <unipafogados@gmail.com>
2019-07-20 11:03:31 -0300

    Atualização final da media

---
+++ App/media.php
@@ -0,0 +1,139 @@
+
+<!DOCTYPE html>
+<html><head>
+    <title>UNIP - Cálculo de Média - Online </title>
+   
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <body>
+    
+    
+    <section class="pricing py-5">
+        <div class="container">
+          <div class="row">           
+
+
+          <div class="col-lg-4">
+            
+            <div div class="card border-danger mb-3" style="max-width: 18rem;">
+                   <div class="card-header text-white text-center bg-danger">BACHARELADOS/LICENCIATURAS</div>
+                
+                           <form class="form-signin">
+                               <br>
+                               <div class="col-auto">
+                  <div class="form-label-group mx-sm-5 mb-3 text-center">
+                        <label>Nota Prova</label>
+                    <input type="number" id="" class="form-control" placeholder="" required autofocus>
+                  </div>
+                  </div>
+                  <div class="col-auto">
+                  <div class="form-label-group mx-sm-5 mb-3 text-center">
+                        <label>Nota AVA</label>
+                        <input type="number" id="" class="form-control" placeholder="" required>
+                  </div>
+                    </div>
+
+                    <div class="col-auto">
+                  <div class="form-label-group mx-sm-5 mb-3 text-center">
+                        <label>Nota Exame</label>
+                        <input type="number" id="" class="form-control" placeholder="" required>
+                   </div>
+                   </div>
+                            </form>
+                            <div class="col-auto">
+                      <div class="form-label-group mx-sm-5 mb-3">
+                <a href="#" class="btn btn-block btn-danger text-uppercase">Calcular!</a>
+                      </div>
+                      </div>
+              </div>
+            </div>
+            
+         
+
+
+                
+    <!--Gestão-->   
+    <div class="col-lg-4">
+                <div div class="card border-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header text-white text-center bg-success ">GESTÃO</div>
+                
+                      <form class="form-signin">
+                          <br>
+                          <div class="col-auto">
+               <div class="form-label-group mx-sm-5 mb-3 text-center">
+              <label>Nota Prova</label>
+                <input type="number" id="" class="form-control" placeholder="" required autofocus>
+               </div>
+                          </div>
+
+                          <div class="col-auto">      
+              <div class="form-label-group mx-sm-5 mb-3 text-center">
+              <label>Nota AVA</label>
+                <input type="number" id="" class="form-control" placeholder="" required>
+              </div>
+                          </div>
+
+                          <div class="col-auto">
+              <div class="form-label-group mx-sm-5 mb-3 text-center">
+              <label>Nota PIM</label>
+                <input type="number" id="" class="form-control" placeholder="" required autofocus> 
+              </div>
+                          </div>
+
+                          <div class="col-auto">
+              <div class="form-label-group mx-sm-5 mb-3 text-center">
+              <label>Nota Exame</label>
+                <input type="number" id="" class="form-control" placeholder="" required>
+              </div>      
+                          </div>
+                  </form>
+                  <div class="col-auto">
+                      <div class="form-label-group mx-sm-5 mb-3">
+                <a href="#" class="btn btn-block btn-success text-uppercase">Calcular!</a>
+                      </div>
+                  </div>
+          </div>
+          </div>
+<!--FIM Gestão -->
+          
+                <div class="col-lg-4">
+              <div div class="card border-primary mb-3" style="max-width: 18rem;">
+                <div class="card-header text-white text-center bg-primary">PÓS</div>
+                
+                <form class="form-signin">
+                    <br>
+                    <div class="col-auto">
+              <div class="form-label-group mx-sm-5 mb-3 text-center">
+              <label>Nota Prova</label>
+                <input type="number" id="" class="form-control" placeholder="" required autofocus>
+                    </div>
+              </div>
+              <div class="col-auto">
+              <div class="form-label-group mx-sm-5 mb-3 text-center">
+              <label>Nota AVA</label>
+              
+                <input type="number" id="" class="form-control" placeholder="" required>
+         
+              </div>
+              </div>
+              </form>
+              <div class="col-auto">
+                      <div class="form-label-group mx-sm-5 mb-3">
+                <a href="#" class="btn btn-block btn-primary text-uppercase">Calcular!</a>
+                </div>
+                </div>
+
+          </div>
+            </div>
+
+          
+
+
+
+        </div>
+    </div>
+  </section>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
